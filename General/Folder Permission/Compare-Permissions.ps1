4589b8e715ed762d6abc93edebb030bd25c0ac6b
TheSleepyAdmin <geffo123@gmail.com>
2020-07-17 22:05:40 +0100

    Create Compare-Permissions.ps1

---
+++ General/Folder Permission/Compare-Permissions.ps1
@@ -0,0 +1,9 @@
+$PreCIFSCheck = Import-Csv -Path "C:\\Pre_OAT_CIFS_Export.csv"
+$PostCIFSCheck = Import-Csv -Path  "C:\Exports\Post_OAT_CIFS_Export.csv"
+$comparePermssions = Compare-Object $PreCIFSCheck $PostCIFSCheck -Property FolderName,FolderPath,IdentityReference,Permissions,AccessControlType
+if ($comparePermssions){
+Foreach ($Folder in $comparePermssions){
+Write-Warning "Permssion missing from $($Folder.FolderName)"
+$Folder | Export-Csv -Path "C:\Exports\Results.csv" -NoTypeInformation -Append
+            }
+    }
