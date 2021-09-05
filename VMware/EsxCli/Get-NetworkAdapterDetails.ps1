e2fc23974c576faece396e34a1b55deeaa2390db
GeoffP <geffo123@gmail.com>
2021-09-05 17:49:00 +0100

    Update Get-NetworkAdapterDetails.ps1
    Added help file and example

--- VMware/EsxCli/Get-NetworkAdapterDetails.ps1
+++ VMware/EsxCli/Get-NetworkAdapterDetails.ps1
@@ -1,26 +1,29 @@
-#------------------------------------------------------------------------------ 
-# THIS CODE AND ANY ASSOCIATED INFORMATION ARE PROVIDED "AS IS� WITHOUT 
-# WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT 
-# LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS 
-# FOR A PARTICULAR PURPOSE. THE ENTIRE RISK OF USE, INABILITY TO USE, OR  
-# RESULTS FROM THE USE OF THIS CODE REMAINS WITH THE USER. 
-# 
-#------------------------------------------------------------------------------ 
-# Author: Geoff Phillis 
-# Version: 1.0
-#------------------------------------------------------------------------------ 
-# VMware NetworkAdapter Details
-# Tested with Powercli 12.0 and above. Requires Powercli install
-# on computer running script. 
-#  
-# ------------------------------------------------------------------------------ 
-# There are no Mandatory parameters only paramter is for reportexport path to
-# export the report to a csv
-# 
-#.EXAMPLE
-# .\Get-NetworkAdapterDetails -ReportExport c:\temp
-#
-# ------------------------------------------------------------------------------
+<#
+.SYNOPSIS
+VMware NetworkAdapter Details
+Author: Geoff Phillis
+Version: 1.0
+
+.DESCRIPTION
+VMware NetworkAdapter Details
+Tested with Powercli 12.0 and above. Requires Powercli install
+To install PowerCli module run
+Install-Module -Name VMware.PowerCLI
+Install-Module -Name VMware.PowerCLI –Scope CurrentUser
+
+.PARAMETER ReportExport
+The ReportExport parameter is an optional paramter to export the report to a csv
+
+.EXAMPLE
+.\VMware_Permissions_Audit.ps1 -VCServer vc.domain.local -ReportExport c:\temp
+
+.NOTES
+THIS CODE AND ANY ASSOCIATED INFORMATION ARE PROVIDED “AS IS” WITHOUT
+WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
+FOR A PARTICULAR PURPOSE. THE ENTIRE RISK OF USE, INABILITY TO USE, OR
+RESULTS FROM THE USE OF THIS CODE REMAINS WITH THE USER.
+#>
 
 param(
     [parameter(Mandatory = $false)]
